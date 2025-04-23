9fccc0004596c2394cdc919d1dceec8d9cf2542f
chris <chris@host.docker.internal>
2025-04-23 18:41:57 +0200

    Reworked

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = requrie('express')();
-
-app.get('/', (req, res) =>
-    res.json({ message: 'Docker is soo easy'})
-);
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => console.log('app listening on 8080'));
