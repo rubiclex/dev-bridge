b065551475d9b6578a0ae414923aaba65d9b86c7
chris <chris@host.docker.internal>
2025-04-27 11:51:14 +0200

    added usere option

--- src/minecraft/commands/platorngCommand.js
+++ src/minecraft/commands/platorngCommand.js
@@ -8,10 +8,20 @@ class platorngCommand extends minecraftCommand {
         this.name = 'platorng';
         this.aliases = ['ptr'];
         this.description = 'Sends your Platow Rng Value';
+        this.options = [
+            {
+                name: 'username',
+                description: 'Minecraft username',
+                required: false
+            }
+        ];
     }
     
     async onCommand(username, message, channel = 'gc') {
         try {
+            
+            let passed_username = this.getArgs(message)[0];
+            username = passed_username || username;
             
             let rngvalue = Math.random() * (100 - 0) + 0;           
             if (username == "Platow") {
