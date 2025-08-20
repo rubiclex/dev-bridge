df65a454cc7981dd35ce54f614e10ee99d6b3016
chris <chris.ruell@rubic-solution.de>
2025-08-20 22:56:39 +0200

    new be

--- src/minecraft/commands/bestiaryCommand.js
+++ src/minecraft/commands/bestiaryCommand.js
@@ -1,96 +1,49 @@
-const { getLatestProfile } = require('../../../API/functions/getLatestProfile.js');
-const { formatUsername } = require('../../contracts/helperFunctions.js');
-const minecraftCommand = require('../../contracts/minecraftCommand.js');
-const { getBestiary } = require('../../../API/stats/bestiary.js');
-const Logger = require('#root/src/Logger.js');
+const { getLatestProfile } = require("../../../API/functions/getLatestProfile.js");
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const { formatNumber } = require("../../contracts/helperFunctions.js");
+const { getBestiary } = require("../../../API/stats/bestiary.js");
 
 class BestiaryCommand extends minecraftCommand {
+    /** @param {import("minecraft-protocol").Client} minecraft */
     constructor(minecraft) {
         super(minecraft);
 
-        this.name = 'bestiary';
-        this.aliases = ['be'];
-        this.description = 'Bestiary of specified user.';
+        this.name = "bestiary";
+        this.aliases = ["be"];
+        this.description = "Bestiary of specified user.";
         this.options = [
             {
-                name: 'username',
-                description: 'Mincraft Username',
+                name: "username",
+                description: "Mincraft Username",
                 required: false
             }
         ];
     }
 
-    async onCommand(username, message, channel = 'gc') {
+    /**
+     * @param {string} player
+     * @param {string} message
+     * */
+    async onCommand(player, message) {
         try {
             const args = this.getArgs(message);
+            player = args[0] ?? player;
 
-            const playerUsername = username;
-            const mob = args[1];
-            username = args[0] || username;
+            const { username, profile } = await getLatestProfile(player);
 
-            const data = await getLatestProfile(username);
-
-            username = formatUsername(username, data.profileData?.game_mode);
-
-            const bestiary = getBestiary(data.profile);
+            const bestiary = getBestiary(profile);
             if (bestiary === null) {
-                return this.send(`/${channel} This player has not yet joined SkyBlock since the bestiary update.`);
+                return this.send("This player has not yet joined SkyBlock since the bestiary update.");
             }
 
-            if (mob) {
-                const mobData = this.getBestiaryObject(bestiary).find((m) =>
-                    m.name.toLowerCase().includes(mob.toLowerCase())
-                );
-
-                if (mobData) {
-                    this.send(
-                        `/${channel} ${username}'s ${mobData.name} Bestiary: ${mobData.kills} / ${mobData.nextTierKills} (${
-                            mobData.nextTierKills - mobData.kills
-                        }) `
-                    );
-
-                    await new Promise((resolve) => setTimeout(resolve, 1000));
-                }
-            }
-
+            const progress = formatNumber((bestiary.level / bestiary.maxLevel) * 100, 2);
             this.send(
-                `/${channel} ${username}'s Bestiary Milestone: ${bestiary.milestone} / ${bestiary.maxMilestone} | Unlocked Tiers: ${bestiary.tiersUnlocked} / ${bestiary.totalTiers}`
+                `${username}'s Bestiary: ${bestiary.level} / ${bestiary.maxLevel} (${progress}%) | Unlocked Tiers: ${bestiary.familyTiers} / ${bestiary.maxFamilyTiers} | Unlocked Families: ${bestiary.familiesUnlocked} / ${bestiary.totalFamilies} | Families Maxed: ${bestiary.familiesCompleted}`
             );
-
-            if (playerUsername === username) {
-                const bestiaryData = this.getBestiaryObject(bestiary).sort(
-                    (a, b) => a.nextTierKills - a.kills - (b.nextTierKills - b.kills)
-                );
-
-                const topFive = bestiaryData.slice(0, 5);
-                const topFiveMobs = topFive.map((mob) => {
-                    return `${mob.name}: ${mob.kills} / ${mob.nextTierKills} (${mob.nextTierKills - mob.kills})`;
-                });
-
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-
-                this.send(`/${channel} Closest to level up: ${topFiveMobs.join(', ')}`);
-            }
         } catch (error) {
-            Logger.warnMessage(error);
-            this.send(`/${channel} [ERROR] ${error}`);
+            console.error(error);
+            this.send(`[ERROR] ${error}`);
         }
-    }
-
-    getBestiaryObject(bestiary) {
-        return Object.keys(bestiary.categories)
-            .map((category) => {
-                if (category === 'fishing') {
-                    Object.keys(bestiary.categories[category]).map((key) => {
-                        if (key === 'name') return;
-                        return bestiary.categories[category][key].mobs.map((mob) => mob);
-                    });
-                } else {
-                    return bestiary.categories[category].mobs.map((mob) => mob);
-                }
-            })
-            .flat()
-            .filter((mob) => mob?.nextTierKills != null);
     }
 }
 
