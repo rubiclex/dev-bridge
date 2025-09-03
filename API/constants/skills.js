681deeee4cd15c1839f8e6d677edc41f811b5953
chris <chris.ruell@rubic-solution.de>
2025-09-03 16:59:02 +0200

    test sa 2

--- API/constants/skills.js
+++ API/constants/skills.js
@@ -1,90 +1,188 @@
-//CREDIT: https://github.com/SkyCrypt/SkyCryptWebsite (Modified)
-const xp_tables = require('./xp_tables.js');
+const { cropTables, skillTables } = require("./leveling.js");
 
-module.exports = function calcSkill(skill, experience, profile=false) {
-    let table = 'normal';
-    if (skill === 'runecrafting') table = 'runecrafting';
-    if (skill === 'social') table = 'social';
-    if (skill === 'dungeoneering') table = 'catacombs';
-    let skill_cap = 100;
+/**
+ * Gets the xp table for the given type.
+ * @param {string} [type='default'] The skill table type. Defaults to the "default" table for skills.
+ * @returns {number[]} xp table
+ */
+function getXpTable(type = "default") {
+    // @ts-ignore
+    return cropTables[type] ?? skillTables[type] ?? skillTables.default;
+}
 
-    if(profile){
-        if (skill === 'farming') {
-            let farming_info = profile?.jacobs_contest?.perks?.farming_level_cap;
-            if (farming_info == undefined) {
-                skill_cap = 50;
-            } else {
-                skill_cap = 50 + farming_info;
-            }
-        }
+/**
+ * Gets the level and some other information from an xp amount.
+ * @param {number} xp The experience points to calculate level information from.
+ * @param {Object} [extra={}] Additional options for level calculation.
+ * @param {string} [extra.type] The ID of the skill (used to determine xp table and default cap).
+ * @param {number} [extra.cap] Override for the highest level the player can reach.
+ * @returns {import("../stats/skills.types.js").Level} The level information.
+ */
+function getLevelByXp(xp, extra = {}) {
+    const xpTable = getXpTable(extra.type);
 
-        if (skill == 'taming') {
-            let taming_cap = (profile?.pets_data?.pet_care?.pet_types_sacrificed ?? []).length + 50;
-            if (taming_cap == undefined) {
-                skill_cap = 50;
-            } else {
-                skill_cap = taming_cap;
-            }
-        }
+    if (typeof xp !== "number" || isNaN(xp)) {
+        xp = 0;
     }
 
-    if (experience <= 0) {
-        return {
-            totalXp: 0,
-            xp: 0,
-            level: 0,
-            xpCurrent: 0,
-            xpForNext: xp_tables[table][0],
-            progress: 0
-        };
-    }
-    let xp = 0;
-    let level = 0;
-    let xpForNext = 0;
-    let progress = 0;
-    let maxLevel = 0;
+    /** the level that this player is caped at */
+        // @ts-ignore
+    const levelCap = extra.cap ?? skillTables.defaultSkillCaps[extra.type] ?? Math.max(...Object.keys(xpTable).map(Number));
 
-    if (xp_tables.max_levels[skill]) maxLevel = xp_tables.max_levels[skill];
+    /** the level ignoring the cap and using only the table */
+    let uncappedLevel = 0;
 
-    for (let i = 1; i <= maxLevel; i++) {
-        xp += xp_tables[table][i - 1];
+    /** the amount of xp over the amount required for the level (used for calculation progress to next level) */
+    let xpCurrent = xp;
 
-        if (xp > experience) {
-            xp -= xp_tables[table][i - 1];
-        } else {
-            if (i <= maxLevel) level = i;
+    /** like xpCurrent but ignores cap */
+    let xpRemaining = xp;
+
+    while (xpTable[uncappedLevel + 1] <= xpRemaining) {
+        uncappedLevel++;
+        xpRemaining -= xpTable[uncappedLevel];
+        if (uncappedLevel <= levelCap) {
+            xpCurrent = xpRemaining;
         }
     }
 
-    if (skill === 'dungeoneering') {
-        level += Math.floor((experience - xp) / 200_000_000);
-        xp += Math.floor((experience - xp) / 200_000_000) * 200_000_000;
+    /** Whether the skill has infinite leveling (dungeoneering and skyblock level) */
+        // @ts-ignore
+    const isInfiniteLevelable = skillTables.infiniteLeveling.includes(extra.type);
 
-        xpForNext = 200000000;
+    /** adds support for infinite leveling (dungeoneering and skyblock level) */
+    if (isInfiniteLevelable) {
+        const maxExperience = Object.values(xpTable).at(-1);
+
+        // @ts-ignore
+        uncappedLevel += Math.floor(xpRemaining / maxExperience);
+        // @ts-ignore
+        xpRemaining %= maxExperience;
+        xpCurrent = xpRemaining;
     }
 
-    const xpCurrent = Math.floor(experience - xp);
+    /** the maximum level that any player can achieve (used for gold progress bars) */
+        // @ts-ignore
+    const maxLevel = isInfiniteLevelable ? Math.max(uncappedLevel, levelCap) : skillTables.maxedSkillCaps[extra.type] ?? levelCap;
 
-    const totalXp = experience;
+    /** the level as displayed by in game UI */
+    const level = isInfiniteLevelable ? uncappedLevel : Math.min(levelCap, uncappedLevel);
 
-    if (level < maxLevel) {
-        xpForNext = Math.ceil(xp_tables[table][level] || 200000000);
-    }
+    /** the amount amount of xp needed to reach the next level (used for calculation progress to next level) */
+    const xpForNext = level < maxLevel ? Math.ceil(xpTable[level + 1] ?? Object.values(xpTable).at(-1)) : isInfiniteLevelable ? Object.values(xpTable).at(-1) : Infinity;
 
-    progress = level >= maxLevel && skill !== 'dungeoneering' ? 0 : Math.max(0, Math.min(xpCurrent / xpForNext, 1));
+    /** the fraction of the way toward the next level */
+        // @ts-ignore
+    const progress = level >= maxLevel && !isInfiniteLevelable ? 0 : Math.max(0, Math.min(xpCurrent / xpForNext, 1));
 
-    //const level = Math.min(Math.floor(profile[skill].levelWithProgress ?? 0), skill_cap);
+    /** a floating point value representing the current level for example if you are half way to level 5 it would be 4.5 */
+    const levelWithProgress = isInfiniteLevelable ? uncappedLevel + progress : Math.min(uncappedLevel + progress, levelCap);
 
-    level = Math.min(level, skill_cap);
-    levelWithProgress = Math.min(level + progress || 0, skill_cap);
+    /** a floating point value representing the current level ignoring the in-game unlockable caps for example if you are half way to level 5 it would be 4.5 */
+    const unlockableLevelWithProgress = extra.cap ? Math.min(uncappedLevel + progress, maxLevel) : levelWithProgress;
+
+    /** the amount of xp needed to max out the skill */
+        // @ts-ignore
+    const maxExperience = getSkillExperience(extra.type, levelCap);
 
     return {
-        totalXp,
         xp,
         level,
+        maxLevel,
         xpCurrent,
+        // @ts-ignore
         xpForNext,
         progress,
-        levelWithProgress: levelWithProgress
+        levelCap,
+        uncappedLevel,
+        levelWithProgress,
+        unlockableLevelWithProgress,
+        maxExperience
     };
+}
+
+/**
+ * Calculates the average skill level for a player's profile data.
+ * @param {import("../../types/profiles.js").Member} profileData
+ * @param {import("../../types/player.js").Player | null} hypixelPlayer
+ * @param {{
+ *  decimals?: number;
+ *  progress?: boolean;
+ *  cosmetic?: boolean;
+ * }} [options={}]
+ * @returns {string} The average skill level.
+ */
+function getSkillAverage(profileData, hypixelPlayer, options = { decimals: 2, progress: false, cosmetic: false }) {
+    const skillLevelCaps = getSkillLevelCaps(profileData, hypixelPlayer);
+
+    let totalLevel = 0;
+    for (const skillId of skillTables.skills) {
+        if (!options.cosmetic && skillTables.cosmeticSkills.includes(skillId)) {
+            continue;
+        }
+
+        // @ts-ignore
+        const skill = getLevelByXp(profileData.player_data?.experience?.[`SKILL_${skillId.toUpperCase()}`], {
+            type: skillId,
+            // @ts-ignore
+            cap: skillLevelCaps[skillId]
+        });
+
+        totalLevel += options.progress ? skill.levelWithProgress : skill.level;
+    }
+
+    const average = totalLevel / skillTables.skills.filter((skill) => !(!options.cosmetic && skillTables.cosmeticSkills.includes(skill))).length;
+
+    return average.toFixed(options.decimals);
+}
+
+/**
+ * Calculates the skill level caps for different skills based on the profile data and Hypixel player data.
+ * @param {import("../../types/profiles.js").Member} profileData
+ * @param {import("../../types/player.js").Player | null} hypixelPlayer
+ * @returns {{
+ *  farming: number;
+ *  taming: number;
+ *  runecrafting: number;
+ * }} An object containing the skill level caps for farming, taming, and runecrafting.
+ */
+function getSkillLevelCaps(profileData, hypixelPlayer) {
+    return {
+        farming: 50 + (profileData.jacobs_contest?.perks?.farming_level_cap || 0),
+        taming: 50 + (profileData.pets_data?.pet_care?.pet_types_sacrificed?.length || 0),
+        runecrafting: 25 // hypixelPlayer?.newPackageRank ? 25 : 3
+    };
+}
+
+/**
+ * Calculates the total experience required to reach a certain level in a skill.
+ * @param {string} skill The ID of the skill used to determine the xp table.
+ * @param {number} level The target level.
+ * @returns {number} The total experience required.
+ */
+function getSkillExperience(skill, level) {
+    const skillTable = getXpTable(skill);
+
+    // @ts-ignore
+    return Object.entries(skillTable).reduce((acc, [key, value]) => (key <= level ? acc + value : acc), 0);
+}
+
+/**
+ * Calculates the total social skill experience for a given profile.
+ * @param {import("../../types/profiles.js").Profile} profile The profile object containing skill data.
+ * @returns {number} The total social skill experience.
+ */
+function getSocialSkillExperience(profile) {
+    return Object.keys(profile.members).reduce((acc, member) => {
+        return acc + (profile.members[member]?.player_data?.experience?.SKILL_SOCIAL || 0);
+    }, 0);
+}
+
+module.exports = {
+    getSkillAverage,
+    getLevelByXp,
+    getXpTable,
+    getSkillLevelCaps,
+    getSkillExperience,
+    getSocialSkillExperience
 };
--- API/stats/skills.js
+++ API/stats/skills.js
@@ -1,31 +1,37 @@
-const calcSkill = require('../constants/skills.js');
+const { getSkillLevelCaps, getSocialSkillExperience, getLevelByXp } = require("../constants/skills.js");
 
-module.exports = function getSkills(profile) {
-    const skill_experience = {
-        farming: profile?.player_data?.experience?.SKILL_FARMING || 0,
-        mining: profile?.player_data?.experience?.SKILL_MINING || 0,
-        combat: profile?.player_data?.experience?.SKILL_COMBAT || 0,
-        foraging: profile?.player_data?.experience?.SKILL_FORAGING || 0,
-        fishing: profile?.player_data?.experience?.SKILL_FISHING || 0,
-        enchanting: profile?.player_data?.experience?.SKILL_ENCHANTING || 0,
-        alchemy: profile?.player_data?.experience?.SKILL_ALCHEMY || 0,
-        carpentry: profile?.player_data?.experience?.SKILL_CARPENTRY || 0,
-        runecrafting: profile?.player_data?.experience?.SKILL_RUNECRAFTING || 0,
-        social: profile?.player_data?.experience?.SKILL_SOCIAL || 0,
-        taming: profile?.player_data?.experience?.SKILL_TAMING || 0,
-    };
+/**
+ * Returns the skill levels for a player's profile data.
+ * @param {import("../../types/profiles.js").Member} profile
+ * @param {import("../../types/profiles.js").Profile} profileData
+ * @returns {import("./skills.types.js").Skills | null}
+ */
+function getSkills(profile, profileData) {
+    if (profile.player_data?.experience === undefined) {
+        return null;
+    }
 
-    return {
-        farming: calcSkill('farming', skill_experience['farming'], profile),
-        mining: calcSkill('mining', skill_experience['mining'], profile),
-        combat: calcSkill('combat', skill_experience['combat'], profile),
-        foraging: calcSkill('foraging', skill_experience['foraging'], profile),
-        fishing: calcSkill('fishing', skill_experience['fishing'], profile),
-        enchanting: calcSkill('enchanting', skill_experience['enchanting'], profile),
-        alchemy: calcSkill('alchemy', skill_experience['alchemy'], profile),
-        carpentry: calcSkill('carpentry', skill_experience['carpentry'], profile),
-        runecrafting: calcSkill('runecrafting', skill_experience['runecrafting'], profile),
-        social: calcSkill('social', skill_experience['social'], profile),
-        taming: calcSkill('taming', skill_experience['taming'], profile)
-    };
+    const skillLevelCaps = getSkillLevelCaps(profile, null);
+    const totalSocialXp = getSocialSkillExperience(profileData);
+
+    /** @type {import("./skills.types.js").Skills} */
+    const skills = {};
+    for (const skill in profile.player_data?.experience || {}) {
+        if (skill === "SKILL_DUNGEONEERING") {
+            continue;
+        }
+
+        // @ts-ignore
+        const xp = skill === "SKILL_SOCIAL" ? totalSocialXp : profile.player_data?.experience[skill];
+        const type = skill.split("_").at(1)?.toLowerCase();
+
+        // @ts-ignore
+        skills[type] = getLevelByXp(xp, { type: type, cap: skillLevelCaps[type] });
+    }
+
+    return skills;
+}
+
+module.exports = {
+    getSkills
 };
